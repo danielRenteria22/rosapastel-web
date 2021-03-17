f5020e01014e6c33657fdc60b82f847591bab6eb
Daniel Renteria <jrenteria@pagando.mx>
2021-03-16 19:43:09 -0700

    Created client registration page

--- utils/bookingsRequests.js
+++ utils/bookingsRequests.js
@@ -22,6 +22,25 @@ async function createForceBooking(start_time,service_id,user_id,name,phone) {
     return await POST('booking/create/force',body)
 }
 
+async function createBooking(start_time,service_id,user_id,name,phone,sms_id,code) {
+    let body = {
+        start_time,
+        service_id,
+        user_id,
+        name,
+        phone,
+        sms_id,
+        code
+    }
+
+    return await POST('booking/create',body)
+}
+
 async function deleteBooking(id) {
     return await DELETE(`booking/${id}`)
+}
+
+async function getUnAvailableIntervals(date) {
+    let body = { date }
+    return await POST('booking/intervals',body)
 }
