378a9e68201a89df297575619cf575c8f2a06bf1
Daniel Renteria <jrenteria@pagando.mx>
2021-09-27 18:49:02 -0600

    Fixed dates in admim screen

--- utils/utils.js
+++ utils/utils.js
@@ -32,11 +32,25 @@ function formatCalendarDate(date) {
     let pieces = letDateString.split(' ')
     let datePieces = pieces[0].split('/')
     let timePieces = pieces[1].split(':')
-    return `${datePieces[2]}-${to2DigitNumber(datePieces[1])}-${to2DigitNumber(datePieces[0])}T${timePieces[0]}:${timePieces[1]}:00-07:00`
+    return `${datePieces[2]}-${to2DigitNumber(datePieces[1])}-${to2DigitNumber(datePieces[0])}T${timePieces[0]}:${timePieces[1]}:00${getOffset()}`
 }
 
 function to2DigitNumber(numerStr) {
     let number = parseInt(numerStr)
     if(number >= 10) return number
     return `0${number}`
+}
+
+function getOffset() {
+    var date = new Date();
+    var offset = date.getTimezoneOffset();
+    let sign = offset >= 0? "-" : "+";
+    let hours = formatTime(Math.floor(offset / 60));
+    let minutes = formatTime(offset % 60)
+    return `${sign}${hours}:${minutes}`
+}
+
+function formatTime(time) {
+    if(time >= 10) return time
+    return `0${time}`
 }
